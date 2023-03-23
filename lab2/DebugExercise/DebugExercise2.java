99564a7b9a34f4a85df560012f39eb651a655581
anonym10048 <764756743@qq.com>
2023-03-23 16:51:59 +0800

    done with lab2, it's so interesting!

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -45,11 +45,13 @@ public class DebugExercise2 {
             return null;
         }
         int[] returnArray = new int[a.length];
-        for (int i = 0; i < a.length; i += 1) {
-            int biggerValue = max(a[i], b[i]);
-            returnArray[i] = biggerValue;
+        for (int i = 0; i < a.length; i++){
+            if (a[i] > b[i]){
+                returnArray[i] = a[i];
+            } else {
+                returnArray[i] = b[i];
+            }
         }
-
         return returnArray;
     }
 
@@ -58,7 +60,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = sum + x[i];
             i = i + 1;
         }
         return sum;
