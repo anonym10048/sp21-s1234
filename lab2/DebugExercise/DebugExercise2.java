338a3a2770d79aa29347cade480647c9cd79b46a
anonym10048 <764756743@qq.com>
2023-03-26 17:13:52 +0800

    done with lab2

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -7,13 +7,7 @@ package DebugExercise;
 public class DebugExercise2 {
     /** Returns the max of a and b. Do not step into this function. */
     public static int max(int a, int b) {
-        int w = (b - a) >> 31;
-        /* If you're stepping into this function, click the
-           step out button because you're not going to learn anything. */
-        int z = ~(b - a) >> 31;
-
-        int max = b & w | a & z;
-        return max;
+        return Math.max(a, b);
     }
 
 
@@ -45,10 +39,11 @@ public class DebugExercise2 {
             return null;
         }
         int[] returnArray = new int[a.length];
-        for (int i = 0; i < a.length; i++){
+        for (int i = 0; i < a.length; i += 1) {
             int biggerValue = max(a[i], b[i]);
             returnArray[i] = biggerValue;
         }
+
         return returnArray;
     }
 
@@ -57,7 +52,7 @@ public class DebugExercise2 {
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + x[i];
+            sum = add(sum, x[i]);
             i = i + 1;
         }
         return sum;
